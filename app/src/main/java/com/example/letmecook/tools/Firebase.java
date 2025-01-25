e2e370d06b43fbe15b434a4f77872eb8bfd1a309
JosephB03 <brennanjoe909@gmail.com>
2025-01-25 23:59:36 +0000

    Added a sample class to use to manipulate database

---
+++ app/src/main/java/com/example/letmecook/tools/Firebase.java
@@ -0,0 +1,38 @@
+package com.example.letmecook.tools;
+
+import static android.content.ContentValues.TAG;
+import android.util.Log;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.*;
+
+import java.util.*;
+
+public class Firebase {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void createUserTest() {
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+}
