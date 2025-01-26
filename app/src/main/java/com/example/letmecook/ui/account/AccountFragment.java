d0ee812403d6c662d66df05bec38e45d079e3c0a
JosephB03 <brennanjoe909@gmail.com>
2025-01-26 23:32:31 +0000

    Added simple non-functional sign out button

--- app/src/main/java/com/example/letmecook/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/letmecook/ui/account/AccountFragment.java
@@ -1,29 +1,40 @@
 package com.example.letmecook.ui.account;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.letmecook.LoginActivity;
+import com.example.letmecook.MainActivity;
 import com.example.letmecook.databinding.FragmentAccountBinding;
 
 public class AccountFragment extends Fragment {
 
-private FragmentAccountBinding binding;
+    private FragmentAccountBinding binding;
+    private Button signOutButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         AccountViewModel accountViewModel =
                 new ViewModelProvider(this).get(AccountViewModel.class);
 
-    binding = FragmentAccountBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentAccountBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
         final TextView textView = binding.textAccount;
         accountViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        signOutButton.setOnClickListener(view -> {
+            signOut();
+        });
+
         return root;
     }
 
@@ -31,5 +42,12 @@ private FragmentAccountBinding binding;
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void signOut() {
+        // Navigate to LoginActivity
+        // TODO implement actual sign out using Firebase. This is simple navigation.
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
     }
 }
