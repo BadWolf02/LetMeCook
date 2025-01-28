387bc56796d291160ecc66e6ac93c6fd140cd87c
JosephB03 <brennanjoe909@gmail.com>
2025-01-28 18:45:49 +0000

    Implemented features from test Firebase auth files into original sign up and login activities.

--- app/src/main/java/com/example/letmecook/FirebaseAuthLoginActivity.java
+++ app/src/main/java/com/example/letmecook/FirebaseAuthLoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.letmecook;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +13,6 @@ public class FirebaseAuthLoginActivity extends AppCompatActivity {
     EditText password;
     Button loginButton;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
