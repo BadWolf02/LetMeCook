2bede25fe44950a7291240e64e46a7fdfab8616b
JosephB03 <brennanjoe909@gmail.com>
2025-01-29 15:37:26 +0000

    Deleted commented out code

--- app/src/main/java/com/example/letmecook/tools/Firebase.java
+++ app/src/main/java/com/example/letmecook/tools/Firebase.java
@@ -28,55 +28,6 @@ public class Firebase {
         this.context = context;
     }
 
-    /*
-    // Method adds a user to the database
-    public boolean createUser(String username, String password) {
-        Map<String, Object> user = new HashMap<>();
-        user.put("username", username);
-        user.put("password", password);
-
-        // Password strength check
-        if (password.length() >= 8) {
-            // Log information on successful sign up
-            // Log information on failed sign up
-            db.collection("users")
-                    .add(user)
-                    .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId()))
-                    .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e));
-
-            Toast.makeText(context, "Sign Up successful!", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        Toast.makeText(context, "Sign Up failed. Password must be at least 8 characters long", Toast.LENGTH_SHORT).show();
-        return false;
-    }
-    */
-    /*
-    public void loginUser(String username, String password) {
-        db.collection("users")
-                .whereEqualTo("username", username)
-                .whereEqualTo("password", password)
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful() && !task.getResult().isEmpty()) { // if success and not empty
-                        // Login successful message
-                        Toast.makeText(context, "Login successful!", Toast.LENGTH_SHORT).show();
-
-                        // Navigate to MainActivity after successful login
-                        Intent intent = new Intent(context, MainActivity.class);
-                        context.startActivity(intent);
-
-                    } else {
-                        // Login failed message
-                        Toast.makeText(context, "Invalid username or password", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(e -> {
-                    // Display errors
-                    Toast.makeText(context, "An error occurred: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                });
-    }
-    */
     // Create user using email and password
     public void addUserAuth(String username, String email, String password) {
         Map<String, Object> user = new HashMap<>();
@@ -165,8 +116,6 @@ public class Firebase {
                         Toast.LENGTH_SHORT).show());
         // [END send_email_verification]
     }
-
-
 
     public boolean isLoggedIn() {
         FirebaseUser currentUser = mAuth.getCurrentUser();
