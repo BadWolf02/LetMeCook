2b378cdd74ca9b50aa6e574f062badfbb828de74
Morgan <122310136@umail.ucc.ie>
2025-02-05 16:56:15 +0000

    CameraActivity now allows the user to take pictures of their recipes and save them to their camera roll

--- app/src/main/java/com/example/letmecook/CameraActivity.java
+++ app/src/main/java/com/example/letmecook/CameraActivity.java
@@ -1,9 +1,19 @@
 package com.example.letmecook;
 
+import android.app.Activity;
+import android.content.ContentValues;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -18,6 +28,7 @@ import java.util.concurrent.ExecutionException;
 
 public class CameraActivity extends AppCompatActivity {
     ProcessCameraProvider cameraProvider;
+    private ImageCapture imageCapture;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +39,14 @@ public class CameraActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        findViewById(R.id.TakePhoto).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+            }
+        });
+
         ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderListenableFuture.addListener(new Runnable() {
             @Override
@@ -46,6 +65,36 @@ public class CameraActivity extends AppCompatActivity {
 
 
     }
+// allows the user to save photos of recipes to their camera roll (can remove if not wanted)
+    private void takePhoto() {
+        if(imageCapture == null) return;
+
+        String name = System.currentTimeMillis() + "";      // timestamp is the name
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P){
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraXStable");
+        }
+        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(
+                getContentResolver(), MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues
+        ).build();
+        imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this), new ImageCapture.OnImageSavedCallback() {
+            @Override
+            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                Toast.makeText(CameraActivity.this, "Image Captured" + outputFileResults.getSavedUri(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+                exception.printStackTrace();
+            }
+        });
+
+    }
+
     private void startCameraX(ProcessCameraProvider cameraProvider){
         CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
 
@@ -53,10 +102,12 @@ public class CameraActivity extends AppCompatActivity {
         PreviewView previewView = findViewById(R.id.preview_view);  // gets the preview_view from XML file
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
+        imageCapture = new ImageCapture.Builder().build();
+
         try {
             cameraProvider.unbindAll();
 
-            cameraProvider.bindToLifecycle(this, cameraSelector, preview);
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
         }catch (Exception e){
             e.printStackTrace();
         }
