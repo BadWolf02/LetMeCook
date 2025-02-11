c5785feb91e8d402bb8dbefbbdf593bc1d87dbcb
JosephB03 <brennanjoe909@gmail.com>
2025-02-11 23:04:58 +0000

    Separated household functionality into a different activity. To be integrated.

--- app/src/main/java/com/example/letmecook/ui/user/UserFragment.java
+++ app/src/main/java/com/example/letmecook/ui/user/UserFragment.java
@@ -1,16 +1,19 @@
 package com.example.letmecook.ui.user;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.letmecook.HouseholdManageActivity;
 import com.example.letmecook.tools.Authentication;
 
 import com.example.letmecook.R;
@@ -27,19 +30,19 @@ public class UserFragment extends Fragment {
                 new ViewModelProvider(this).get(UserViewModel.class);
 
         Authentication auth = new Authentication(requireContext());
-        Household household = new Household(requireContext());
+        // Household household = new Household(requireContext());
 
         binding = FragmentUserBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSearchMember;
+        final TextView textView = binding.textUser;
         userViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         // TODO implement checkbox / force user to click on the household first before inviting
-        EditText householdInput = binding.getRoot().findViewById(R.id.householdInput);
-        EditText searchBar = binding.getRoot().findViewById(R.id.searchBar);
-        Button searchButton = binding.getRoot().findViewById(R.id.searchButton);
-        searchButton.setOnClickListener(view -> household.inviteUser(householdInput.getText().toString(), searchBar.getText().toString()));
+        // EditText householdInput = binding.getRoot().findViewById(R.id.householdInput);
+        // EditText searchBar = binding.getRoot().findViewById(R.id.searchBar);
+        // Button searchButton = binding.getRoot().findViewById(R.id.searchButton);
+        // searchButton.setOnClickListener(view -> household.inviteUser(householdInput.getText().toString(), searchBar.getText().toString()));
 
         Button signOutButton = binding.getRoot().findViewById(R.id.sign_out);
         signOutButton.setOnClickListener(view -> auth.signOut());
