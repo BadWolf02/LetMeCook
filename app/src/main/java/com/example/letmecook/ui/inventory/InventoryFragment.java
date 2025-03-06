1764e9f73f5781c33a7efdcd8dbe2196542cba05
Jack Weitzman <jacksamweitzman@gmail.com>
2025-03-06 11:18:10 +0000

    Removed hard coded UID value

--- app/src/main/java/com/example/letmecook/ui/inventory/InventoryFragment.java
+++ app/src/main/java/com/example/letmecook/ui/inventory/InventoryFragment.java
@@ -24,6 +24,7 @@ import com.example.letmecook.adapters.InventoryAdapter;
 import com.example.letmecook.databinding.FragmentInventoryBinding;
 import com.example.letmecook.db_tools.SearchDB;
 import com.example.letmecook.models.Ingredient;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -75,7 +76,7 @@ public class InventoryFragment extends Fragment {
         searchDB = new SearchDB();
 
         // Fetch and display inventory
-        String userID = "Exb0LIB0vIMVYJN2kAKHO0zd1mD2"; // Set your actual user ID
+        String userID = FirebaseAuth.getInstance().getUid();; // Set your actual user ID
         loadUserInventory(userID);
 
         return root;
